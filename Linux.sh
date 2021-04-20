1d0eb64479c8abd11779df5aedca6775114505d6
2Timeowl <78010388+Technocyber@users.noreply.github.com>
2021-04-20 12:54:58 +0530

    Create Linux.sh

---
+++ Linux.sh
@@ -0,0 +1,28 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+# Author: Technocyber
+# Made with ❤️
+
+echo
+echo -e "\e[1;31mThis Tool Will Install Kali Linux in your Termux"
+echo
+sleep 2.0
+echo -e "\e[0;32mImportant Instructions Before Installing Kali Linux"
+echo
+echo -e "1) Don't keep termux in background"
+echo -e "2) Don't let your display sleep"
+echo -e "3) Good Internet speed"
+echo -e "4) Patience : Will take time"
+sleep 2.0
+echo -e "\e[0;34mInstallation Process Starting"
+sleep 2.0
+termux-setup-storage
+apt install wget -y
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
+echo -e "Thanks For Using This Tool"
+echo -e "\e[1;90mLove From TECHNOCYBER"
+echo -e "Loading....."
+echo " "
